f6ca0f8661e9521e6e21ada0d47c473740c3616e
martinwairegi <martinwairegikimani@gmail.com>
2021-12-14 11:45:06 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+// react
+import React, { useState, useEffect } from 'react';
+
+// openlayers
+import GeoJSON from 'ol/format/GeoJSON'
+import Feature from 'ol/Feature';
+
+// components
+import MapWrapper from './components/MapWrapper'
+
+function App() {
+  
+  // set intial state
+  const [ features, setFeatures ] = useState([])
+
+  // initialization - retrieve GeoJSON features from Mock JSON API get features from mock 
+  //  GeoJson API (read from flat .json file in public directory)
+  useEffect( () => {
+
+    fetch('/mock-geojson-api.json')
+      .then(response => response.json())
+      .then( (fetchedFeatures) => {
+
+        // parse fetched geojson into OpenLayers features
+        //  use options to convert feature from EPSG:4326 to EPSG:3857
+        const wktOptions = {
+          dataProjection: 'EPSG:4326',
+          featureProjection: 'EPSG:3857'
+        }
+        const parsedFeatures = new GeoJSON().readFeatures(fetchedFeatures, wktOptions)
+
+        // set features into state (which will be passed into OpenLayers
+        //  map component as props)
+        setFeatures(parsedFeatures)
+
+      })
+
+  },[])
+  
+  return (
+    <div className="App">
+      
+      <div className="app-label">
+        <p>Create a GIS app with React Functional Components</p>
+        <p>Click the map to reveal location coordinate via React State</p>
+      </div>
+      
+      <MapWrapper features={features} />
+
+    </div>
+  )
+}
+
+export default App
